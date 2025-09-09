65b8590d9c24c82079ef8e69e76203f08fe1ef7f
Ayaa-anya <reshamaepigao.scc@gmail.com>
2025-09-09 15:02:14 +0800

    Part2Seatwork11.java

---
+++ Part2Seatwork11.java
@@ -0,0 +1,7 @@
+public class Part2Seatwork11 {
+  public static void main(String[] args) {
+    int[][] myNumbers = { {1, 4, 2}, {3, 6, 8} };
+    myNumbers[1][2] = 9;
+    System.out.println(myNumbers[1][2]); 
+  }
+}
